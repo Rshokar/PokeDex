a1740a570b6638869b4efd4ef2de07ed0053d8a6
Ravinder Shokar <ravindershokar@gmail.com>
2023-04-02 22:13:29 -0700

    Got pokemon from DB

--- client/src/controllers/PokemonController.ts
+++ client/src/controllers/PokemonController.ts
@@ -1,10 +1,19 @@
 import axios from 'axios'
+import Pokemon from '../models/Pokemon'
 
 export class PokemonController {
 
 
     static async getPokedex(): Promise<any> {
 
+        axios.get<Pokemon[]>('http://localhost:5000/pokemons')
+            .then(response => {
+                const todos = response.data;
+                console.log(todos);
+            })
+            .catch(error => {
+                console.error(error);
+            });
     }
 
 }
--- client/src/screens/Dashboard/index.tsx
+++ client/src/screens/Dashboard/index.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import style from './style'
 import HalfCard from '../../components/Cards/HalfCard'
+import Pokemon from '../../models/Pokemon'
+import { PokemonController } from '../../controllers/PokemonController'
 
 
 const Dashboard = () => {
 
+    const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+
+    useEffect(() => {
+        async function run(): Promise<any> {
+            const pk = await PokemonController.getPokedex();
+            console.log(pk)
+        }
+
+        run()
+    }, [])
     const { container } = style;
     return (
         <HalfCard >
