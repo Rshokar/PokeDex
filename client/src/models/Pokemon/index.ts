9d241149aa80e8dcdb60b467d016160b1f6ccab4
Ravinder Shokar <ravindershokar@gmail.com>
2023-04-02 22:05:26 -0700

    Added pokemone type

---
+++ client/src/models/Pokemon/index.ts
@@ -0,0 +1,31 @@
+
+interface PokemonBase {
+    HP: number
+    Attack: number
+    Defense: number
+    Speed: number
+    "Speed Attack": number
+    "Speed Defense": number
+}
+
+interface PokemonName {
+    english: string
+    japanese: string
+    chinese: string
+    french: string
+}
+
+class Pokemon {
+    base: PokemonBase
+    name: PokemonName
+    id: number
+    type: string[]
+    constructor(bs: PokemonBase, id: number, name: PokemonName, type: string[]) {
+        this.base = bs
+        this.id = id
+        this.name = name
+        this.type = type
+    }
+}
+
+export default Pokemon
--- client/src/screens/Dashboard/index.tsx
+++ client/src/screens/Dashboard/index.tsx
@@ -2,9 +2,8 @@ import React from 'react'
 import style from './style'
 import HalfCard from '../../components/Cards/HalfCard'
 
-type Props = {}
 
-const Dashboard = (props: Props) => {
+const Dashboard = () => {
 
     const { container } = style;
     return (
