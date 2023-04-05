809cf9a0fe93ac10b5e9a953062ff157941f2dff
Ravinder Shokar <ravindershokar@gmail.com>
2023-04-04 22:08:50 -0700

    Added pagintion to dashboard page

--- client/src/screens/Dashboard/index.tsx
+++ client/src/screens/Dashboard/index.tsx
@@ -10,19 +10,24 @@ import Button from '../../components/Forms/Button'
 const Dashboard = () => {
 
     const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+    const [page, setPage] = useState<number>(0);
+    const [limit, setLimit] = useState<number>(9);
 
     useEffect(() => {
         async function run(): Promise<any> {
-            const pk: Pokemon[] = await PokemonController.getPokedex();
-            setPokemon(pk);
+            const pk: Pokemon[] = await PokemonController.getPokedex(page, limit);
+            setPokemon([...pokemon, ...pk]);
         }
         run()
-    }, [])
+    }, [page, limit])
 
     return (
         <HalfCard >
             <Images pokemon={pokemon} />
-            <Button style={{ width: '100%' }}>
+            <Button style={{ width: '100%' }} onClick={(e) => {
+                e.preventDefault();
+                setPage(page + 1)
+            }}>
                 Load More
             </Button>
         </HalfCard>
